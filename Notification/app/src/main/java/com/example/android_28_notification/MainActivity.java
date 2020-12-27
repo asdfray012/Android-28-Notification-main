8515df270c6ffe0c5bd250bab036f64457c27577
3A613120 <asdfray012@gmail.com>
2020-12-27 14:19:24 +0800

    Android-28-Notification

---
+++ Notification/app/src/main/java/com/example/android_28_notification/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android_28_notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    // new 一個 method
+    public void bt1(View view) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("嗨嗨")
+                .setContentText("我的老天");
+        Notification notification = builder.build();
+        notificationManager.cancel(0); // 移除id值為0的通知
+        notificationManager.notify(0, notification);
+    }
+}
